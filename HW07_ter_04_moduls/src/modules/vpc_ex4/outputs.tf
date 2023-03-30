7a27585aec4b71f10a5276fad1300bf31cff279b
Aleksey Shelkovin <ashelkovin@gmail.com>
2023-03-31 01:53:25 +0300

    [HW07-ter-04-modules] v2.3

--- HW07_ter_04_moduls/src/modules/vpc_ex4/outputs.tf
+++ HW07_ter_04_moduls/src/modules/vpc_ex4/outputs.tf
@@ -4,7 +4,6 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  count = yandex_vpc_subnet.new_subnet
-  value = [ for subnet in yandex_vpc_subnet.new_subnet : subnet.id ] 
+  value = [ for subnet in yandex_vpc_subnet.new_subnet : subnet.id ]
   description = "subnets id"
 }
