64c85a42b418beefb9e7ab914586c6b1322fc013
Aleksey Shelkovin <ashelkovin@gmail.com>
2023-03-31 01:10:45 +0300

    [HW07-ter-04-modules] v2

--- HW07_ter_04_moduls/src/hw_terraform/main.tf
+++ HW07_ter_04_moduls/src/hw_terraform/main.tf
@@ -1,12 +1,17 @@
 #создаем облачную сеть
+
+/*
+# звдвние 2
 module "vpc_dev" {
   source = "../modules/vpc"
   vpc_name     = var.vpc_name
   default_cidr = var.default_cidr
   default_zone = var.default_zone
 }
+*/
 
 /*
+#Задание 1
 resource "yandex_vpc_network" "develop" {
   name = "develop"
 }
@@ -18,8 +23,26 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = ["10.0.1.0/24"]
 }
-
 */
+
+#Задание 4
+module "vpc_prod" {
+  source       = "../modules/vpc"
+  vpc_name     = "production"
+  subnets = [
+    { zone = "ru-central1-a", cidr = ["10.0.1.0/24"] },
+    { zone = "ru-central1-b", cidr = ["10.0.2.0/24"] },
+    { zone = "ru-central1-c", cidr = ["10.0.3.0/24"] },
+  ]
+}
+
+module "vpc_dev" {
+  source       = "../modules/vpc"
+  vpc_name     = "develop"
+  subnets = [
+    { zone = "ru-central1-a", cidr = ["10.0.1.0/24"] },
+  ]
+}
 
 module "test-vm" {
   source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
@@ -37,7 +60,6 @@ module "test-vm" {
   metadata = {
       user-data          = data.template_file.cloudinit.rendered
       serial-port-enable = 1
-      #ssh-keys           = file(var.public_key)
   }
 
 }
--- HW07_ter_04_moduls/src/modules/vpc/variables.tf
+++ HW07_ter_04_moduls/src/modules/vpc/variables.tf
@@ -1,3 +1,4 @@
+/*
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
@@ -8,11 +9,19 @@ variable "default_cidr" {
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
-
+*/
 variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
 }
 
+variable "subnets" {
+  type	      = list(object)
+  default     = [
+    { zone = "ru-central1-a", cidr = [ "10.0.1.0/24" ] }
+  ]
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope &
+    https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
 
