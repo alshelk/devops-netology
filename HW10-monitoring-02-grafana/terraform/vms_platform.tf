7924d9df1ac3f687b12d4cdce5ad98fb7569aa80
Aleksey Shelkovin <ashelkovin@gmail.com>
2023-07-24 08:26:55 +0300

    [HW10-monitoring-02-grafana] v0.0.2

---
+++ HW10-monitoring-02-grafana/terraform/vms_platform.tf
@@ -0,0 +1,47 @@
+variable "vm_web_family" {
+  type        = string
+  default     = "centos-7"
+  description = "OS family & release"
+}
+
+variable "vm_less9" {
+  type      = object({
+    vm_name=list(string),
+    cpu=number,
+    ram=number,
+    disk=number,
+    core_fraction = number
+  })
+  default = {
+      vm_name = ["prometheus-01", "grafana-01", "exporter-01"],
+      cpu = 2,
+      ram = 4,
+      disk = 20,
+      core_fraction = 20
+  }
+}
+
+variable "vm_common_arg" {
+  type        = object({
+    platform_id       = string,
+    scheduling_policy = map(bool),
+    network_interface = map(bool)
+  })
+  default     = {
+    platform_id = "standard-v1",
+    scheduling_policy = { preemptible = true },
+    network_interface = { nat = true }
+  }
+  description = "resource instance variables"
+}
+
+variable "vm_metadata" {
+  type      = object({
+    serial-port-enable = number
+    ssh-user          = string
+  })
+  default   = {
+    serial-port-enable = 1
+    ssh-user           = "centos"
+  }
+}
