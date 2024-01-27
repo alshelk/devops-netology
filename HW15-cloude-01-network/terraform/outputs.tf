c6f44f544789769c2f6383a8882157219395ad8e
Aleksey Shelkovin <ashelkovin@gmail.com>
2024-01-27 21:45:09 +0300

    HW15-cloude-01-network

--- HW15-cloude-01-network/terraform/outputs.tf
+++ HW15-cloude-01-network/terraform/outputs.tf
@@ -4,7 +4,6 @@ output "route_table" {
 
 
 output "instances" {
-#     value = yandex_compute_instance.nat_instance
      value = [{ "ip" = yandex_compute_instance.nat_instance.network_interface[0].ip_address, "name" = yandex_compute_instance.nat_instance.name },
               { "ext_ip" =  yandex_compute_instance.test_vm_public.network_interface[0].nat_ip_address, "ip" = yandex_compute_instance.test_vm_public.network_interface[0].ip_address, "name" = yandex_compute_instance.test_vm_public.name },
               { "ext_ip" =  yandex_compute_instance.test_vm_private.network_interface[0].nat_ip_address, "ip" = yandex_compute_instance.test_vm_private.network_interface[0].ip_address, "name" = yandex_compute_instance.test_vm_private.name }]
